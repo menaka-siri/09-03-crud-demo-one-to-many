a73fb4c9d9520061f48ecaf4155b12949772802f
Menaka S <menaka442@gmail.com>
2024-12-15 00:36:06 +0530

    initial commit

---
+++ src/main/java/com/zealtrack/crud_demo/Application.java
@@ -0,0 +1,75 @@
+package com.zealtrack.crud_demo;
+
+import com.zealtrack.crud_demo.dao.AppDAO;
+import com.zealtrack.crud_demo.entity.Instructor;
+import com.zealtrack.crud_demo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+		return runner -> {
+			System.out.println("Hi, Mom");
+//			createInstructor(appDAO);
+//			findInstructor(appDAO);
+			deleteInstructor(appDAO);
+		};
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+		int theId =1;
+		System.out.println("Deleting instrcutor id: " + theId);
+
+		appDAO.deleteInstructorById(theId);
+
+		System.out.println("Done!");
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+		int theId = 1;
+		System.out.println("Finding instructor id: " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associated instructorDetail only: " + tempInstructor.getInstructorDetail());
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+
+		Instructor tempInstructor = new Instructor("Chad", "Darby", "darby@luv2code.com");
+
+		InstructorDetail tempInstructorDetail = new InstructorDetail(
+				"http://www.luv2code.com/youtube",
+				"Luv 2 code!!!"
+		);
+
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+
+//		Instructor tempInstructor = new Instructor("Madhu2", "Patel2", "madhu2@luv2code.com");
+//
+//		InstructorDetail tempInstructorDetail = new InstructorDetail(
+//				"http://www.luv2code.com/youtube",
+//				"Guitar"
+//		);
+//
+//		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+
+		System.out.println("Saving instructor: " + tempInstructor);
+
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done");
+	}
+}
---
+++ src/main/java/com/zealtrack/crud_demo/dao/AppDAO.java
@@ -0,0 +1,12 @@
+package com.zealtrack.crud_demo.dao;
+
+import com.zealtrack.crud_demo.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+}
